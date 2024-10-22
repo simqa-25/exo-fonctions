c458a14d0fc60414b7b76c087af28ced34171906
simqa-25 <zeyacin@live.fr>
2024-10-22 14:54:02 +0200

    seond commit

--- index.js
+++ index.js
@@ -1,195 +1,352 @@
-const prompt = require("prompt-sync")()
+const prompt = require('prompt-sync')()
 
-// EXO 1
+// let test = ["abricot", "fraise", "mangue", "pasteque"]
+// for (let index = 0; index < test.length; index++) {
 
-
-// let choice = prompt("Entrez un prenom entre Alice , Pierre, Quentin, Louis ");
-// function sayHello(name) {
-//     if (name === "Alice") {
-//         return "Bonjour " + name ;
-//     }else {
-//         return"Je te dis pas bonjour";  
+//     if (test[index] == "mangue") {
+//         console.log(test[index]);
+//         break;
 //     }
 // }
-// console.log(sayHello(choice));
 
-// EXO 2
 
-// let choice = parseInt(prompt("Entrez un age :  "))
+// TD-4 tableaux
 
-// function isadult(age) {
-//     if (age >= 18) {
-//         return true
+
+// // EXO 1
+// let choices = []
+// for (let j = 0; j < 5; j++) {
+//     let choice = Number(prompt(" Entrez un nombre : " ))    
+//     choices.push(choice)
+// }
+
+
+
+//  function sommeTableau(chiffres) {
+//     let total = 0
+
+//     for (let i = 0; i < chiffres.length; i++) {
+//         total = total + chiffres[i]
+
 //     }
-//     else {
-//         return false
-//     }
-// }console.log(isadult(choice));
-
-
-// EXO 3
-
-// let choice = prompt("Entrez un mot :  ")
-// function inverserMot(word) {
-//     return word.split("").reverse().join("");
-
+// return total
 // }
-// console.log("Le mot ecris à l'envers donne " + inverserMot(choice));
+// console.log(sommeTableau(choices));
 
 
+// EXO 2 
 
-// Correction
-
-
-// let choice = prompt("Entrez un mot, ou une phrase :  ")
-
-
-// function reverseStr(string) {
-//     let result = ""
-//     for (let i = string.length-1; i >= 0; i--) {
-//         result = result + string[i]
-//     }
-//     return result
+// let choices = []
+// for (let j = 0; j < 5; j++) {
+//     let choice = Number(prompt(" Entrez un nombre : "))
+//     choices.push(choice)
 // }
-// console.log(("Voici votre mot ou votre phrase inverse :  "),reverseStr(choice));
-
-// EXO 4
 
 
-// function genererNombreAleatoire() {
-//     return Math.floor(Math.random() * 100) + 1 ;
-// }
-// function inverserChiffres(nombre) {
-//     return parseInt(nombre.toString().split("").reverse().join(""), 10)
-// }
-// let nombreAleatoire = genererNombreAleatoire();
-// console.log(" Le nombre aléatoire genere est le  :  " +  nombreAleatoire);
-// let nombreInverse = inverserChiffres(nombreAleatoire);
-// console.log(nombreInverse);
 
-// TD 3
-
-// EXO 1
-
-// let choice = prompt("Entrez un mot, ou une phrase :  ")
-
-
-// function countLetters(string) {
-//     let compteur = 0;
-
-//     for (let i = 0; i < string.length - 1; i++) {
-//         let letters = string[i]
-//         if ((letters >= "a" && letters <= "z") || (letters >= "A" && letters <= "Z")) {
-//             compteur++
+// function getEvenNumbers(chiffres) {
+//     let evenNumbers = []
+//     for (let i = 0; i < chiffres.length; i++) {
+//         if (isEven(chiffres[i])) {
+//             evenNumbers.push(chiffres[i])
 //         }
 //     }
-//     return compteur;
+//     return evenNumbers
 // }
-// console.log(("Voici votre mot ou votre phrase contient :  "), countLetters(choice) + " lettres . ");
-
-// EXO 2
-
-// let choice = prompt("Entrez un mot, ou une phrase :  ")
-// let times = Number(prompt("Entrez le nombre de fois que vous souhaitez repeter votre mot ou phrase :  "))
-
-// function repeat(string, number) {
-//     if (number < 0) {
-//         return
-//     }
-//     let result = "";
-//     for (let i = 1; i <= times; i++) {
-//         result += string
-//     }
-//     return result
+// function isEven(number) {
+//     return number % 2 === 0
 // }
-// console.log(repeat(choice));
+
+// console.log(getEvenNumbers(choices));
 
 
 // EXO 3
 
-// let choice = prompt("Entrez un mot, ou une phrase :  ")
-
-
-// function reverseStr(string) {
-//     let result = ""
-//     for (let i = string.length-1; i >= 0; i--) {
-//         result = result + string[i]
-//     }
-//     return result
+// let choices = []
+// for (let j = 0; j < 5; j++) {
+//     let choice = Number(prompt(" Entrez un nombre : "))
+//     choices.push(choice)
 // }
-// console.log(("Voici votre mot ou votre phrase inverse :  "),reverseStr(choice));
+
+// function bigger(chiffres) {
+//     let max = 0
+//     for (let i = 0; i < chiffres.length - 1; i++) {
+
+
+//         if (max > chiffres[i] && max > chiffres[i + 1]) {
+//             continue
+//         }
+//         if (chiffres[i] > chiffres[i + 1]) {
+//             max = chiffres[i]
+//         }
+//         else {
+//             max = chiffres[i + 1]
+//         }
+//     }
+//     return max
+// }
+// console.log(bigger(choices));
+
+// VERSION 2 :
+
+
+// function bigger(chiffres) {
+//     let max = chiffres[0]
+//     for (let i = 1; i < chiffres.length; i++) {
+//         if (max < chiffres[i]) {
+//             max = chiffres[i]
+//         }
+//     }
+//     return max
+// }
+
+
+// console.log(bigger(choices));
 
 
 // EXO 4
 
-// let size = prompt("Veuillez entrez votre taille en m, (ex : si vous mesurez 1.85m vous tapez 1.85), :  ")
-// let weight = parseFloat(prompt("Veuillez rentrez votre poids en kg, (vous pouvez mettre votre poids exacte ex : 81.5,)  :  "))
-// let result = calculImc(weight, size)
-// function calculImc(weight, size) {
-//     let result = weight / (size * size)
-//     return result.toFixed(2)
-
-// }
-// console.log("Votre IMC est de :  ", + result);
-// if (result <= 18.5) {
-//     console.log("Il s'agirait de manger maintenant. Vous etes maigre");
-// } else if (result > 18.5 && result <= 25) {
-//     console.log("Sois normal mec");
-// }
-// else if (result > 25 && result <= 30) {
-//     console.log("Fais belek tu commences a etre en surpoids");
-// }
-// else if (result > 30 && result <= 40) {
-//     console.log("Sa commence a faire beaucoup la non ? bienvenue chez les obeses");
-// }
-// else {
-//     console.log("Mec c'est vraiment chaud la, mais ne perds pas espoir tu peux y arriver!");
+// let choices = []
+// for (let j = 0; j < 5; j++) {
+//     let choice = Number(prompt(" Entrez un nombre : "))
+//     choices.push(choice)
 // }
 
-
-
-// EXO 5 
-
-
-// let name = prompt("Veuillez entrer un nom : ")
-// let firstname = prompt("Veuillez entrer un prenom : ")
-// let birthdate = prompt("Veuillez entrer une annee de naissance : ")
-
-// function genererPseudo(name, firstname, birthdate) {
-// return   name + firstname + birthdate 
+// function reverse(chiffres) {
+//     let reversedArray = []
+//     for (let i =  chiffres.length - 1; i >= 0 ; --i) {
+//      reversedArray.push(chiffres[i])  
+//     }
+//     return reversedArray
 // }
-// console.log(genererPseudo(name,firstname,birthdate));
+// console.log(reverse(choices));
 
-// EXO 6
+// TD 4 LES TABLEAUX
 
-let firstname = prompt("Veuillez entrer un prenom : ")
-let hour = prompt("Veuillez  entrer une heure ")
-let minutes = prompt("Veuillez  entrer des minutes ")
+// EXO 1
 
-function saluerPersonne(name, time) {
-    if (time <= 0 && <= 7) {
-        return 
+// let choices = []
+// for (let j = 0; j < 5; j++) {
+//     let choice = Number(prompt(" Entrez un nombre : " ))    
+//     choices.push(choice)
+// }
+
+
+
+//  function sommeTableau(chiffres) {
+//     let total = 0
+
+//     for (let i = 0; i < chiffres.length; i++) {
+//         total = total + chiffres[i]
+
+//     }
+// return total
+// }
+// console.log(sommeTableau(choices));
+
+
+// EXO 2
+
+// let choices = []
+// for (let j = 0; j < 5; j++) {
+//     let choice = Number(prompt(" Entrez un nombre : "))
+//     choices.push(choice)
+// }
+
+// function sommeTableau(chiffres) {
+//     let total = 0
+
+//     for (let i = 0; i < chiffres.length; i++) {
+//         total += chiffres[i]
+
+//     }
+//     return total
+// }
+
+// function produitTableau(chiffres) {
+//     let total = 1
+//     for (let k = 0; k < chiffres.length; k++) {
+//         total *= chiffres[k]
+
+//     }
+//     return total
+// }
+
+// function moyenneTableau(chiffres) {
+//     let total = sommeTableau(chiffres);
+//     return total / chiffres.length;
+// }
+
+// let somme = sommeTableau(choices);
+// let produit = produitTableau(choices);
+// let moyenne = moyenneTableau(choices);
+
+
+// console.log("Somme:", somme);
+// console.log("Produit:", produit);
+// console.log("Moyenne:", moyenne);
+
+// EXO 3
+
+// let choices = []
+// for (let j = 0; j < 5; j++) {
+//     let choice = Number(prompt(" Entrez un nombre : " ))    
+//     choices.push(choice)
+// }
+// let time = Number(prompt("Entrez un chiffre de 0 à 9 : "))
+
+
+//  function timesAppear(chiffres, target) {
+//     let compteur = 0;
+
+//     for (let i = 0; i < chiffres.length; i++) {
+//         if (chiffres[i] === target) {
+//             compteur++
+//         }
+
+//     }
+// return compteur
+// }
+// let result = timesAppear(choices, time);
+// console.log(`Le chiffre ${time} apparaît ${result} fois.`);
+
+
+// EXO 4
+
+// let choices = [];
+// for (let j = 0; j < 5; j++) {
+//     let choice = Number(prompt("Entrez un nombre : "));
+//     choices.push(choice);
+// }
+
+// let newElement = Number(prompt("Entrez un nouveau nombre à insérer : "));
+// let position = prompt("À quelle position voulez-vous l'insérer ? (début, milieu, fin) : ").toLowerCase();
+
+// function insertElement(array, element, pos) {
+//     if (pos === "debut") {
+//         array.unshift(element); 
+//     } else if (pos === "milieu") {
+//         let middleIndex = Math.floor(array.length / 2);
+//         array.splice(middleIndex, 0, element); 
+//     } else if (pos === "fin") {
+//         array.push(element); 
+//     } else {
+//         console.log("Position invalide. Aucune insertion effectuée.");
+//     }
+// }
+
+// insertElement(choices, newElement, position);
+// console.log("Tableau après insertion :", choices);
+
+
+// EXO 5
+
+// function removeEL(tab, el) {
+//     let result = []
+//     for (let i = 0; i < tab.length; i++) {
+//      if (tab[i] != el) {
+//         result.push(tab[i])
+//      }
+
+//     }
+//     return result
+// }
+// console.log(removeEL([5, 4, 8, 9, 10],9));
+
+
+
+// EXO 6 
+
+// [5, 8, 9, 7, 6]
+
+// function triABulle(tab) {
+//     for (let i = 0; i < tab.length; i++) {
+//         let minIndex = i
+
+//         for (let j = i + 1; j < tab.length; j++) {
+//             if (tab[j] < tab[minIndex]) {
+//                 minIndex = j
+//             }
+
+//         }
+//         if (minIndex != i) {
+//             let temp = tab[i]
+//             tab[i] = tab[minIndex]
+//             tab[minIndex] = temp
+//         }
+//     }
+//     return tab
+// }
+
+// console.log(triABulle[5, 8, 9, 7, 6]);
+
+
+
+
+// function schtroumpf(tab1, tab2) {
+//     let result = 0
+//     for (let i = 0; i < tab1.length; i++) {
+// for (let j = 0; j < tab2.length; j++) {
+// result = result + tab1[i] *tab2[j]
+    
+// }
         
-    }
-    }
+//     }
+//    return result
+// }
+
+
+// console.log(schtroumpf([2, 4, 5],[3, 1]));
+
+
+
+// function classe(tab) {
+//     let sum = 0
+//     let moy = 0
+//     let max = 0
+//     let pos = null
+//     let supNmb = 0
+//     for (let i = 0; i < tab.length; i++) {
+//       sum = sum + tab[i]
+//       if (tab[i] > max) {
+//         max = tab[i]
+//         pos = i + 1 
+//       }
+        
+//     }
+//     moy = sum / tab.length
+//     for (let j = 0; j < tab.length; j++) {
+//         if (tab[i] > moy) {
+//             supNmb ++
+//         }
+        
+//     }
+//     return "La moyenne de la classe est de : " + moy + "la note maximum est de : " + max + "a la "
+// }
+
+
+
+// MATRICE
+
+// let listCourse = [
+//     ["courgette", "concombre", "citrouille"],
+// ["pomme", "poire", "kiwi"],
+// ["lait", "oeuf", "couche"]
+// ]
+
+
+// for (let i = 0; i < listCourse.length; i++) {
+// for (let j = 0; j < listCourse[i].length; j++) {
+//     console.log(listCourse[i][j]);
+    
+    
+// }
+    
+// }
 
 
 
 
-// EXO 7
+// exo matrice
 
-let choice = prompt("Entrez un mot, ou une phrase :  ")
-
-
-function countLetters(string) {
-    let compteur = 0;
-
-    for (let i = 0; i < string.length - 1; i++) {
-        let letters = string[i]
-        if (letters = "a" && "e" && "i" && "o" && "u" && "y" ) && (letters = "A" && "E" && "I" && "O" && "U" && "Y"); {
-            compteur++
-        }
-    }
-    return compteur;
-}
-console.log(("Voici votre mot ou votre phrase contient :  "), countLetters(choice) + " lettres . ");
+// let questions = ["Quelle est la capitale d'Italie ? ", "Quel est le composant dont le symbole est O ? "]
